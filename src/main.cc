d427962f9b1008dd7df331939b3ffbf02874ab61
leobus1205 <reo.akutsu.bus@gmail.com>
2020-12-18 19:56:40 +0900

    Add main. It's empty now.

---
+++ src/main.cc
@@ -0,0 +1,11 @@
+#include "convert_output2duty.hpp"
+#include "descrete_pid_controller.hpp"
+#include "kalman_filter.hpp"
+#include "motor_control.hpp"
+#include "mpu9250.hpp"
+
+int main()
+{
+
+    return 0;
+}
